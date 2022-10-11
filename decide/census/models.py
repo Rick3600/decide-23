af0f0814e64af96a5085b4400c874c51c592c6e9
ricnadgar <nadalrick@gmail.com>
2022-10-11 17:06:26 +0200

    Practica 1

--- decide/census/models.py
+++ decide/census/models.py
@@ -1,9 +1,10 @@
+from sys import maxsize
 from django.db import models
 
 
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
-
+    adscription = models.CharField(max_length= 30)
     class Meta:
-        unique_together = (('voting_id', 'voter_id'),)
+        unique_together = (('voting_id', 'voter_id','adscription'),)
